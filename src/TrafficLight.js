c727d4ff188155481eaa0a2490b7d83c2058bcbe
Alejae1998 <alejagr1998@gmail.com>
2022-06-09 16:02:31 -0700

    all functions working

--- src/TrafficLight.js
+++ src/TrafficLight.js
@@ -7,11 +7,11 @@ export default function TrafficLight({ color }) {
       {}
       {/* in each of these divs, set it up so that if the color matches, add the css class 'on' to the classNames */}
       {/* for example, the css classes for the div below should be `red circle on`, if props.color is 'red' */}
-      <div className={`red circle ${color.color === 'red' ? 'on' : ''}`}></div>
+      <div className={`red circle ${color === 'red' ? 'on' : ''}`}></div>
       {/* the css classes for the div below should be `yellow circle on`, if color.color is 'yellow' */}
-      <div className={`yellow circle`}></div>
+      <div className={`yellow circle ${color === 'yellow' ? 'on' : ''}`}></div>
       {/*  the css classes for the div below should be `green circle on`, if color.color is 'green' */}
-      <div className={`green circle`}></div>
+      <div className={`green circle ${color === 'green' ? 'on' : ''}`}></div>
     </div>
   );
 }
